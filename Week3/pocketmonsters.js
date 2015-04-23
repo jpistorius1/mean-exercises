c33760f7c693960481c9b885429a77752335154d
Jerry Pistorius <jpistorius1@gmail.com>
2015-04-22 20:52:28 -0600

    Bug fixes

--- Week3/pocketmonsters.js
+++ Week3/pocketmonsters.js
@@ -1,19 +1,20 @@
 var ask = require('readline-sync');
 var colors = ["blue", "red", "yellow", "green", "turquiose"];
 var moods = ["hungry", "happy", "bored"];
-var monsterHealth = 100;
+var FULL_HEALTH = 100; 
+//var monsters = undefined;
 
 var monster = function(color){
-	this.health = monsterHealth;
+	this.health = FULL_HEALTH;
 	this.mood = "happy";
 	this.color = color; 
 };
 
 var startOfGame = function(){
-	var myMonster = generateMonster();
+	monster = generateMonster();
 	timerOfRequest();
-	console.log("You have a new Monster! You're lucky and got a " + myMonster.color);
-	console.log("Your monster has " + myMonster.health + "% and is " + myMonster.mood);
+	console.log("You have a new Monster! You're lucky and got a " + monster.color);
+	console.log("Your monster has " + monster.health + "% and is " + monster.mood);
 };
 
 var generateMonster = function(){
@@ -23,66 +24,71 @@ var generateMonster = function(){
 	return newMonster;	
 };
 
+startOfGame();
+
 function timerOfRequest(){
 	var fed = false;
 	var bored = false;
 	setInterval(function () {
-		if (myMonster.mood == moods[0]){
-			checkFed(fed);
-			fed = false;
-		}
-
-		console.log(myMonster.health);
 
 		var newMood = moods[Math.floor(Math.random() * 3)];
-		myMonster.mood = newMood;
+		monster.mood = newMood;
 		console.log("Your monster is " + newMood);
 
-	if (myMonster.mood == [0]){
+	if (monster.mood == moods[0]){
 		var answer = ask.question("Would you like to feed your pet? Yes or No? ");
 		if (answer.toLowerCase() == "yes"){
 			fed = true;
 		} else {
-			(answer.toLowerCase() == "no");
+			answer.toLowerCase() == "no";
 			fed = false;
+			checkFed(fed);
 			}
 		} 
-	} else (myMonster.mood == [2]){
-		var answer = ask.question("Would you like to play a game with your pet? Yes or No? ");
-		if (answer.toLowerCase() == "yes"){
-			bored = true;
-		} else {
-			(answer.toLowerCase() == "no"){
-			bored = false;
-		}
+	else if (monster.mood == moods[1]) {
+		
+		console.log("You're doing an amazing job. Keep up the great work! ");
 	}
-
-
+	else {
+		(monster.mood == moods[2])
+		var answerOfGame = ask.question("Would you like to play a game with your pet? Yes or No? ");
+		if (answerOfGame.toLowerCase() == "yes"){
+			notBored = true;
+		} else {
+			answerOfGame.toLowerCase() == "no";
+			notBored = false;
+			checkBored(bored);
+		}
+	} 
 	}, 2000);
 } 
 
 function checkFed(fed){
 	if (fed == true){
-		myMonster.health = 100;
+		monster.health += 10;
 	} else {
-		myMonster.health -= 10; 
+		monster.health -= 10;
+		console.log(monster.health);  
+		checkHealth();
 	}
 }
 
 
-function checkBored(bored){
-	if (bored == true){
-		myMonster.moods = [2];
+function checkBored(notBored){
+	if (notBored == true){
+		monster.health += 10;
 	} else {
-		myMonster.health -= 10; 
+		monster.health -= 10;
+		console.log(monster.health); 
+		checkHealth();
 	}
 }
 
 function checkHealth(){
-	if (myMonster.health == 0){
-		var endOfTheGame = ask.question("Would you like to start over? ")
+	if (monster.health == 0){
+		var endOfTheGame = ask.question("You let them die!!!! Would you like to start over? ")
 		if (endOfTheGame.toLowerCase() == 'yes'){
 			startOfGame();
-		}
+		} 
 	}
 }
